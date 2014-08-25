af7fc14433f6d91887b6753093b82d936b92537f
clemsos <clement.renaud@gmail.com>
2014-08-25 11:57:54 +0200

    init

---
+++ ENV.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# download Weiboscope path
+export TOPOGRAM_RAW_DATA_PATH=`pwd`
+
+# tmp files
+export TOPOGRAM_TMP_PATH="/tmp"
+
+# elasticsearch
+export TOPOGRAM_ES_HOST="localhost:9200"
+
+# mongo DB
+export TOPOGRAM_MONGO_HOST='localhost'
+export TOPOGRAM_MONGO_PORT=27017
+export TOPOGRAM_MONGO_DB='weiboscope'
